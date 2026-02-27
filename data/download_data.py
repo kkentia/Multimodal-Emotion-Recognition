e0b6732c67b8265006c267fb32c7d2f46a9ff25c
Ana Bo <ana.bog@unifr.ch>
2026-02-27 16:27:21 +0100

    download script for the datasets

---
+++ data/download_data.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+import kagglehub
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+RAW_DIR = os.path.join(BASE_DIR, "raw")
+
+os.makedirs(RAW_DIR, exist_ok=True)
+
+DATASETS = {
+    "raf-db": "shuvoalok/raf-db-dataset",
+    "fer2013": "msambare/fer2013",
+    "ravdess": "orvile/ravdess-dataset",
+    "cremad": "ejlok1/cremad"
+}
+
+def download_and_move():
+    print(f"Files will be saved to: {RAW_DIR}\n")
+    
+    for folder_name, kaggle_path in DATASETS.items():
+        target_path = os.path.join(RAW_DIR, folder_name)
+        
+        # skip if already downloaded
+        if os.path.exists(target_path) and len(os.listdir(target_path)) > 0:
+            continue
+            
+        print(f" Downloading {folder_name}...")
+        # kagglehub downloads to a cache and returns the cache path
+        cache_path = kagglehub.dataset_download(kaggle_path)
+        
+        print(f"Copying {folder_name} to local repository...")
+        shutil.copytree(cache_path, target_path, dirs_exist_ok=True)
+        print(f"{folder_name} successfully copied.\n")
+
+if __name__ == "__main__":
+    download_and_move()
+    print("All raw datasets are in data/raw/")
